4824e7b60b159a03d71c2390815d0cee05b96c34
Jonathan Beaulieu <jo@smarthalo.bike>
2020-09-04 14:08:36 -0400

    Share.py: Transfère des func today(), tomorrow(), infivedays() et ohlc_as_tuple() vers Historical_stock.py. Extraction de la partie fonctionnel de get_todays_close_df() de ARIMA_prediction(). Transfère vers Hist... Intro de lag arg dans ARIMA_prediction.

--- FinForecast/Share.py
+++ FinForecast/Share.py
@@ -3,87 +3,33 @@ from yahoofinancials import YahooFinancials
 import pandas as pd
 import smtplib, ssl
 from datetime import date, timedelta
-from FinForecast import ArimaForecast as af
 import pickle
+
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from pprint import pprint
 from googleapiclient import discovery
+from pprint import pprint
+
+from FinForecast import ArimaForecast as af
+from FinForecast import Historical_stock as hist
 
 
-# Init script to grab historical data base.
-"""
-    yahoo_financials = YahooFinancials(ticker)
-    data = yahoo_financials.get_historical_price_data(start_date='2020-03-17',  
-                                                    end_date='2020-08-19',  
-                                                    time_interval='daily') 
-
-    df_historical = pd.DataFrame(data[ticker]['prices'])
-    col = list(df_historical.columns)
-    col[-1] = 'Date'
-    df_historical.columns = col
-    df_historical = df_historical.drop('date', axis=1).set_index('Date')
-    df_historical = df_historical.drop(columns=['high', 'low', 'open', 'volume', 'adjclose'])
-    df_historical['rtn5'] = df_historical['close'].pct_change(5) * 100
-    df_historical['meanDev'], df_ravg = af.tfrm_to_MinusMean(df_historical['rtn5'])
-    df_historical.dropna(inplace=True)
-
-    df_historical.to_pickle('_Output/TROX_2020_pour_input_arima.pkl')
-    """
-
-##################################################################################
-
-def today():
-    today = date.today()
-    return today
-
-def tomorrow(today= today()):
-    delay = timedelta(days=1)
-    tomorrow = today + delay
-    return tomorrow
-
-def infivedays(today= today(), delay= timedelta(days=7)):
-    date_infivedays = today + delay
-    return date_infivedays
-
-def get_todays_close(ticker):
-    """Returns a simple dataframe of today's date as index and ticker's close as value.
-    Parameter:
-        ticker (str): Official stock market ticker. Ex.: 'TROX'
-    Exemple:
-                    close
-        2020-09-04   9.27
-    """
-    # Get today's close price
-    yahoo_financials = YahooFinancials(ticker)
-    data_today = yahoo_financials.get_historical_price_data(start_date= str(today()),  
-                                                            end_date= str(tomorrow()),  
-                                                            time_interval='daily') 
-
-    # Extracting the data out of the dictionnary
-    date = [data_today[ticker]['prices'][-1]['formatted_date']]
-    close = [data_today[ticker]['prices'][-1]['close']]
-
-    # Create today's df with today's data
-    df_today = pd.DataFrame(data= close, index= date, columns= ['close'])
-
-    return df_today
-
-def ARIMA_prediction(df_today, df_historical, path_to_df_historical, ARIMA_order= (3,0,6), window_size=92):
+def ARIMA_prediction(df_today, df_historical, path_to_df_historical, ARIMA_order= (3,0,6), window_size=92, lag= 5):
     """
     Take a dataframe of the historical data of a stock to return ARIMA price foretcast in 5 days.
     """
+    return_label = 'rtn{}'.format(lag)
 
     # Add today's data to historical data
     df_historical = df_historical.append(df_today)
 
-    # Calcul today's return with 5 days ago
+    # Calcul today's return with {lag} days ago
     todays_close = df_historical['close'].iloc[-1]
     five_days_ago_close = df_historical['close'].iloc[-6]
     todays_return5 = ( ( todays_close / five_days_ago_close ) - 1) * 100 
-    df_historical['rtn5'].iloc[-1] = todays_return5
+    df_historical[return_label].iloc[-1] = todays_return5
 
     # Transform (find) today's mean deviation return
     todays_transformed_return = todays_return5 - df_historical.iloc[-12:, 1].mean()
@@ -102,10 +48,8 @@ def ARIMA_prediction(df_today, df_historical, path_to_df_historical, ARIMA_order
 
     return predicted_price_in_five_days.iloc[0]
 
-
 def by_mail(subject, message, sender_email= 'finboys.news@gmail.com', receiver_email=  'finboys.news@gmail.com', smtp_ssl= "smtp.gmail.com"):
-    """
-    Send a simple email to recipient through secure SSL.
+    """Send a simple email to recipient through secure SSL.
     """
     
     port = 465  # For SSL
@@ -124,38 +68,14 @@ def by_mail(subject, message, sender_email= 'finboys.news@gmail.com', receiver_e
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
 
-    
-def ohlc_as_tuple(ticker, start_date= '2020-01-01', end_date= str(tomorrow())):
-    
-    yahoo_financials = YahooFinancials(ticker)
-    data_today = yahoo_financials.get_historical_price_data(start_date= start_date,  
-                                                            end_date= end_date,  
-                                                            time_interval= 'daily') 
-
-    date = [ i['formatted_date'] for i in data_today[ticker]['prices']]
-    high = [ i['high'] for i in data_today[ticker]['prices']]
-    low = [ i['low'] for i in data_today[ticker]['prices']] 
-    open_ = [ i['open'] for i in data_today[ticker]['prices']]
-    close_ = [ i['close'] for i in data_today[ticker]['prices']]
-    volume = [ i['volume'] for i in data_today[ticker]['prices']]
-    adjclose = [ i['adjclose'] for i in data_today[ticker]['prices']]
-
-    ohlc_tuple =  tuple(zip(date, high, low, open_, close_, volume, adjclose))
-    ohlc_list = [list(i) for i in ohlc_tuple]
-
-    return ohlc_list
-
-
 def by_ggsheet(values, file_id, range_, value_input_option= 'RAW', insert_data_option= 'OVERWRITE'):
+    """Add (append) values to google spreadsheet.
 
-    """
-    Add (append) values to google spreadsheet.
-
-    Parameters:
-        values (list): list of values to append. https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values
-        file_id (str): Spreadsheet URL between d/ ... /edit
-        range_: A1 notation of the range of field to apply append method. Will be first empty row of 
-            column A anyway because it's what spreadsheets.values.append does.
+        Parameters:
+            values (list): list of values to append. https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values
+            file_id (str): Spreadsheet URL between d/ ... /edit
+            range_: A1 notation of the range of field to apply append method. Will be first empty row of 
+                column A anyway because it's what spreadsheets.values.append does.
     """
 
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
