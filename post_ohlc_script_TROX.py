e1284d8a9dea5edbe16434b9273d827fe7e30efa
Jonathan Beaulieu <jo@smarthalo.bike>
2020-09-04 14:12:11 -0400

    Update des script de prod pour intégrer les derniers changements aux modules.

--- post_ohlc_script_TROX.py
+++ post_ohlc_script_TROX.py
@@ -1,11 +1,11 @@
 from FinForecast import Share
-from FinForecast import Historical_stock as hist
+from FinForecast import Historical_stock as Hist
 #import pandas as pd
 
 
 ticker = 'TROX'
 
-data = hist.get_ohlc_as_tuple(ticker)
+data = Hist.get_ohlc_as_tuple(ticker)
 Share.by_ggsheet(data, 
                 '1fj0fJu8DEYuLXNiefXLWOqExtEQZjX5I4eMhHGz-jJI', 
                 'TROX_data')
