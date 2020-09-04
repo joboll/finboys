a616d8f289a3788a1907a91377be8e01c25ea6a1
Jonathan Beaulieu <jo@smarthalo.bike>
2020-09-04 05:33:05 -0400

    pred_emailer_script_TROX.py: Ajustment to new Share.get_todays_close().

--- pred_emailer_script_TROX.py
+++ pred_emailer_script_TROX.py
@@ -1,16 +1,19 @@
 from FinForecast import Share
 import pandas as pd
 
-
 ticker = 'TROX'
 path_to_df_historical = '~/Documents/finboys/_Output/TROX_2020_pour_input_arima.pickle'
 df_historical = pd.read_pickle(path_to_df_historical)
 
-pred = Share.ARIMA_prediction(ticker, df_historical, path_to_df_historical, ARIMA_order= (2,0,5))
+df_today = Share.get_todays_close(ticker)
+pred = Share.ARIMA_prediction(df_today, df_historical, path_to_df_historical, ARIMA_order= (2,0,5))
 
 subject = 'Prediction du jour'
 message = "Le modele predit que {} cloturera a {:.2f} USD le {}".format(ticker, pred, str(Share.infivedays()))
-destinataires = ['francoisroyca@gmail.com', 'victor.m.cerdacarvajal@gmail.com',' thomas.brown.05@gmail.com', 'finboys.news@gmail.com']
+destinataires = ['francoisroyca@gmail.com', 
+                'victor.m.cerdacarvajal@gmail.com',
+                'thomas.brown.05@gmail.com', 
+                'finboys.news@gmail.com'
+                ]
 
 Share.by_mail(subject, message, receiver_email=destinataires)
-
