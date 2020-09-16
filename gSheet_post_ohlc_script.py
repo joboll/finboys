32c2b41db0914ef029e28f87d3606ad6d2fb28a0
Jonathan Beaulieu <jo@smarthalo.bike>
2020-09-15 23:40:15 -0400

    gSheet_post_ohlc_script for prop

---
+++ gSheet_post_ohlc_script.py
@@ -0,0 +1,12 @@
+from FinForecast import Share
+from FinForecast import Historical_stock as Hist
+#import pandas as pd
+
+
+ticker = 'TROX'
+
+data = Hist.get_ohlc_as_tuple(ticker)
+Share.by_ggsheet(data, 
+                '1fj0fJu8DEYuLXNiefXLWOqExtEQZjX5I4eMhHGz-jJI', 
+                'TROX_data')
+
